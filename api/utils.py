e9cfc4fec4cab6dd50d94f288cba5cd82ff7fbaa
mcgin374 <mcgin374@umn.edu>
2023-06-08 20:46:23 -0500

    frontend recieves array of params

--- api/utils.py
+++ api/utils.py
@@ -27,35 +27,35 @@ def graph_data(pulse_type):
     print(f'pulse_type = {pulse_type}')
 
     if pulse_type == 'sinc':
-        print("graph_data: in sinc")
-
         nlobes = 5
         ydata = np.sin((nlobes+1) * np.pi * tau ) / ((nlobes+1) * np.pi * tau)
         ydata[np.isnan(ydata)] = 1.0 # correct div by zero error
 
     elif pulse_type == 'gauss':
-        print("graph_data: in gauss")
-
         # Here is the calculation of the guassian shape
         truncation_sigma = 3
         ydata = np.exp(-0.5 * (tau * truncation_sigma)**2)
 
     else:
-        print("graph_data: no type defined")
         ydata = np.array([4.01, 2.0, 1.3, 1])
         xdata = np.array([1, 2, 3, 4])
 
-    # xdata = np.array2string(xdata, formatter={'float_kind':lambda x: '%.3f' % x})
-    # ydata = np.array2string(ydata, formatter={'float_kind':lambda x: '%.3f' % x})
-
     xdata = format_numpy_as_json_list(xdata)
     ydata = format_numpy_as_json_list(ydata)
-
-    print(xdata)
-    print(ydata)
 
     return {
         # Note: if np thinks the arrays are floats it will call this formatter. 
         'xdata': xdata,
         'ydata': ydata
     }   
+
+def base_graph_params(pulse_type):
+    params = "[]"
+
+    if pulse_type == 'w_sinc':
+        # Sorry for the super long line, Python doesn't like multi-line JSON
+        params = '[{"name": "npts", "val": "128", "min": "28", "max": "512", "step": "16"}, {"name": "duration", "val": "2.0", "min": "0.1", "max": "20.0", "step": "0.1"}, {"name": "nlobes", "val": "5", "min": "1", "max": "11", "step": "1"}, {"name": "window_alpha", "val": "1.0", "min": "0.001", "max": "1", "step": "0.001"}]'
+    elif pulse_type == 'gauss':
+        params = '[{"name": "npts", "val": "128", "min": "28", "max": "512", "step": "16"}, {"name": "duration", "val": "2.0", "min": "0.1", "max": "20.0", "step": "0.1"}, {"name": "trunc", "val": "3.0", "min": "0.5", "max": "10", "step": "0.5"}]'
+
+    return params
