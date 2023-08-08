7d76812099628bdf701b8f2a565ea333f26967c8
patbolan <79389100+patbolan@users.noreply.github.com>
2023-08-07 20:38:11 -0500

    content type checking
    Was a problem with the content type in the POST request. Should be json, but if sent as text, is now converted to json.

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, json
 from utils import graph_data, base_graph_params
 
 app = Flask(__name__, static_url_path="", static_folder='../client/build')
@@ -19,9 +19,25 @@ def get_pulse():
         'graph_params': base_graph_params('none')
     }
 
+# PJB: Adding this function because sometimes (presumably due to a configuration difference)
+# The reqests get sent with Content-type = 'text/plain;charset=UTF-9'. Best to fix this
+# in the client, but for now, just convert that text to json
+def get_json_data_from_request(request):
+    # PJB adding code to convert request from text to json if needed
+    content_type = request.headers.get('Content-type')
+    if content_type.startswith('text'):
+        print(f'*** WARNING: Expecting json, getting request with Content-type of {content_type}')
+        json_data = json.loads(request.data)
+    else:
+        json_data = request.get_json(silent=True)
+
+    return json_data
+
 @app.route("/pulsechange", methods=['POST', 'GET'])
 def change_pulse():
-    pulse_type = request.get_json(silent=True)['type']
+
+    json_data = get_json_data_from_request(request)
+    pulse_type = json_data['type']
     params = base_graph_params(pulse_type)
 
     return {'type': pulse_type,
@@ -31,10 +47,12 @@ def change_pulse():
 
 @app.route("/pulsegraphparamchange", methods=['POST', 'GET'])
 def change_graph_param():
-    pulse_type = request.get_json(silent=True)['type']
-    params = (request.get_json(silent=True)['graph_params'])
+    json_data = get_json_data_from_request(request)
+    pulse_type = json_data['type']
+    params = (json_data['graph_params'])
 
     return {
             'graph_params': params,
             'graph_data': graph_data(pulse_type, params)
             }
+
